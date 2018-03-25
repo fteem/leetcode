2df0550118e2de5608c34799f2ea5c3bf2edc52e
Bart de Goede <b.de.goede@catawiki.nl>
2018-03-25 21:56:16 +0200

    This is very similar to 26; shift non-val elements forwards

---
+++ 27. Remove Element/solution.py
@@ -0,0 +1,27 @@
+def swap(nums, p1, p2):
+    tmp = nums[p1]
+    nums[p1] = nums[p2]
+    nums[p2] = nums[p1]
+
+def removeElement(nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    if not nums:
+        return 0
+
+    p1 = 0
+    for p2 in range(0, len(nums)):
+        if nums[p2] != val:
+            nums[p1] = nums[p2]
+            p1 += 1
+    return p1
+
+if __name__ == '__main__':
+    assert(removeElement([3, 2, 2, 3], 3) == 2)
+    assert(removeElement([], 0) == 0)
+    assert(removeElement([1], 1) == 0)
+    assert(removeElement([3, 3], 3) == 0)
+    assert(removeElement([4, 5], 5) == 1)
