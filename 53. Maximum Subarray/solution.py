2f734983e4936c9a264960279a7ea0e44d23ea35
Bart de Goede <b.de.goede@catawiki.nl>
2018-04-05 08:59:18 +0200

    solution with two sums

--- 53. Maximum Subarray/solution.py
+++ 53. Maximum Subarray/solution.py
@@ -3,8 +3,15 @@ def maxSubArray(nums):
     :type nums: List[int]
     :rtype: int
     """
-    return 6
+    if not nums:
+        return 0
 
+    max_sum = current_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
 
 if __name__ == '__main__':
     assert(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
