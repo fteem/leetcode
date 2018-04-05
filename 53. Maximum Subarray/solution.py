883e23340f429657e78b76fba80518c6fbc2fa23
Bart de Goede <b.de.goede@catawiki.nl>
2018-04-05 08:50:29 +0200

    README

---
+++ 53. Maximum Subarray/solution.py
@@ -0,0 +1,10 @@
+def maxSubArray(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    return 6
+
+
+if __name__ == '__main__':
+    assert(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
