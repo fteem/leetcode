b02b99c2de353a2d8cc5525ce7f12d356c157551
Bart de Goede <b.de.goede@catawiki.nl>
2018-06-07 23:12:56 +0200

    In order traversal

---
+++ 100. Same Tree/solution.py
@@ -0,0 +1,10 @@
+def is_same_tree(p, q):
+    if p == q:
+        return True
+    if p is None or q is None:
+        return False
+
+    if p.val != q.val:
+        return False
+
+    return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
