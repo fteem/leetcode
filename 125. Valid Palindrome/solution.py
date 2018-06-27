d728c3f060d228d6b210f38fa561e273d51d4b5f
Bart de Goede <b.de.goede@catawiki.nl>
2018-06-27 22:46:55 +0200

    Python cheats

--- 125. Valid Palindrome/solution.py
+++ 125. Valid Palindrome/solution.py
@@ -17,8 +17,19 @@ def is_palindrome(s):
     return True
 
 
+def python_palindrome_cheat(s):
+    # Iterates over the string twice tho
+    s = ''.join([c for c in s.lower() if c.isalnum()])
+    return s == s[::-1]
+
+
 if __name__ == '__main__':
-    assert(is_palindrome('not a palindrome') == False)
-    assert(is_palindrome('racecar') == True)
-    assert(is_palindrome('A man, a plan, a canal: Panama') == True)
-    assert(is_palindrome('race a car') == False)
+    assert(is_palindrome('not a palindrome') is False)
+    assert(is_palindrome('racecar') is True)
+    assert(is_palindrome('A man, a plan, a canal: Panama') is True)
+    assert(is_palindrome('race a car') is False)
+
+    assert(python_palindrome_cheat('not a palindrome') is False)
+    assert(python_palindrome_cheat('racecar') is True)
+    assert(python_palindrome_cheat('A man, a plan, a canal: Panama') is True)
+    assert(python_palindrome_cheat('race a car') is False)
